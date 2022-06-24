1d035ef19a435de615a54177b774d79db3103b98
Yogeshpanta <yogeshpanta33@gmail.com>
2022-06-24 10:15:21 +0545

    the prototype

--- lib/home_page/home_page_one.dart
+++ lib/home_page/home_page_one.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uzme/home_page/drawer.dart';
 //import 'package:uzme/home_page/general_info.dart';
 import 'package:uzme/sign_log_in_page/log_in_page.dart';
 //import 'package:uzme/sign_log_in_page/sign_in_page.dart';
@@ -16,141 +17,130 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xffF8FCFC),
 
-      body:Column(
-        //mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Row(
-            children: [
+      body:SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
 
-              Container(
+                Container(
 
-                padding: const EdgeInsets.only( left: 10),
-              child: IconButton(
-                icon: const Icon(Icons.menu, size: 50,),
-                tooltip: 'Show Snackbar',
-                onPressed:  ( ) {
-
-                },
-              ),
-              ),
-              Padding(padding: const EdgeInsets.only(right: 55, left: 55, top: 80),
-              child: Container(
-              height: 150,
-                width: 150,
-                alignment: Alignment.topCenter,
-                padding: const EdgeInsets.all(20),
-                decoration:  const BoxDecoration(
-                 // borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/logo.png',),
-                    fit: BoxFit.fill,
-                  ),
-                  shape: BoxShape.circle,
+                  padding: const EdgeInsets.only( left: 10),
+                child: IconButton(
+                  icon: const Icon(Icons.menu, size: 50,),
+                  tooltip: 'Show Snackbar',
+                  onPressed:  ( ) {
+                      const DrawerPage();
+                  },
                 ),
-              ),
-              ),
-            ],
-          ),
-        const Padding(
-          padding:EdgeInsets.only(right: 20) ,
-         child: Text('Welcome, People',
-           style: TextStyle(
-           fontSize: 28,
-           fontWeight: FontWeight.w500,
-         ),),
-    ),
+                ),
+                Padding(padding: const EdgeInsets.only(right: 55, left: 55, top: 80),
+                child: Container(
+                height: 150,
+                  width: 150,
+                  alignment: Alignment.topCenter,
+                  padding: const EdgeInsets.all(20),
+                  decoration:  const BoxDecoration(
+                   // borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/logo.png',),
+                      fit: BoxFit.fill,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                ),
+
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child:  IconButton(onPressed:  () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content:
+                        Text('Vechile will be there by tommorrow')));
+                  },
+                    icon: Icon(
+                      Icons.add_alert_rounded,
+                    ),),
+                )
+              ],
+            ),
           const Padding(
-            padding:EdgeInsets.only(right: 0) ,
-            child: Text('use me to complete',
+            padding:EdgeInsets.only(right: 20) ,
+           child: Text('Welcome, People',
+             style: TextStyle(
+             fontSize: 28,
+             fontWeight: FontWeight.w500,
+           ),),
+    ),
+            const Padding(
+              padding:EdgeInsets.only(right: 0) ,
+              child: Text('use me to complete',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),),
+            ),
+            const Text('your garbage goal',
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 15,
                 fontWeight: FontWeight.w500,
               ),),
-          ),
-          const Text('your garbage goal',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w500,
+
+            const Padding(padding: EdgeInsets.only(right:285,top: 50),
+            child:Text('Schedule',
+            ),
+            ),
+            Card(
+              elevation: 5,
+              margin: const EdgeInsets.only(right: 19, left: 19),
+              child: Container(
+               // margin: EdgeInsets.only(right: 19, left: 19),
+                height: 38,
+                width: double.infinity,
+                color: const Color(0xffD9D9D9),
+                padding: const EdgeInsets.only(top:10),
+                child: const Text('      Organic: Today oct 12  5.00 PM',
+                textAlign: TextAlign.center,
             ),),
+            ),
+            Card(
+              elevation: 5,
+              margin: const EdgeInsets.only(right: 19, left: 19, top: 10),
+              child: Container(
+                // margin: EdgeInsets.only(right: 19, left: 19),
+                height: 38,
+                width: double.infinity,
+                color: const Color(0xffD9D9D9),
+                padding: const EdgeInsets.only(top:10),
+                child: const Text('Ingorganic: oct 15  8.00 AM',
+                  textAlign: TextAlign.center,
+                ),),
+            ),
 
-          const Padding(padding: EdgeInsets.only(right:285,top: 50),
-          child:Text('Schedule',
-          ),
-          ),
-          Card(
-            elevation: 5,
-            margin: const EdgeInsets.only(right: 19, left: 19),
-            child: Container(
-             // margin: EdgeInsets.only(right: 19, left: 19),
-              height: 38,
-              width: double.infinity,
-              color: const Color(0xffD9D9D9),
-              padding: const EdgeInsets.only(top:10),
-              child: const Text('      Organic: Today oct 12  5.00 PM',
-              textAlign: TextAlign.center,
-          ),),
-          ),
-          Card(
-            elevation: 5,
-            margin: const EdgeInsets.only(right: 19, left: 19, top: 10),
-            child: Container(
-              // margin: EdgeInsets.only(right: 19, left: 19),
-              height: 38,
-              width: double.infinity,
-              color: const Color(0xffD9D9D9),
-              padding: const EdgeInsets.only(top:10),
-              child: const Text('Others: oct 15  8.00 AM',
-                textAlign: TextAlign.center,
-              ),),
-          ),
 
-          Card(
-            elevation: 5,
-            margin: const EdgeInsets.only(right: 19, left: 19, top: 10),
-            child: Container(
-              // margin: EdgeInsets.only(right: 19, left: 19),
-              height: 38,
-              width: double.infinity,
-              color: const Color(0xffD9D9D9),
-              padding: const EdgeInsets.only(top:10),
-              child: const Text('Reusable: oct 15  8.00 AM',
-                textAlign: TextAlign.center,
-              ),),
-          ),
 
-          Card(
-            elevation: 5,
-            margin: const EdgeInsets.only(right: 19, left: 19, top: 10,
-                bottom: 20),
-            child: Container(
-              // margin: EdgeInsets.only(right: 19, left: 19),
-              height: 38,
-              width: double.infinity,
-              color: const Color(0xffD9D9D9),
-              padding: const EdgeInsets.only(top:10),
-              child: const Text('Recyleable: oct 17  8.00 AM',
-                textAlign: TextAlign.center,
-              ),),
-          ),
 
-           ElevatedButton(
-                onPressed: ( ) {
+
+
+             ElevatedButton(
+                  onPressed: ( ) {
     Navigator.push(context, MaterialPageRoute(
     builder:(context)=> const Donates()));
     },
-              child: const Text('Donations',
+                child: const Text('Donations',
+                  textAlign: TextAlign.center,),
+                ),
+            ElevatedButton(
+              onPressed: ( ) {
+                Navigator.push(context, MaterialPageRoute(
+                    builder:(context)=> const LogInPage()));
+              },
+              child: const Text(' Sign out  ',
                 textAlign: TextAlign.center,),
-              ),
-          ElevatedButton(
-            onPressed: ( ) {
-              Navigator.push(context, MaterialPageRoute(
-                  builder:(context)=> const LogInPage()));
-            },
-            child: const Text(' Sign out  ',
-              textAlign: TextAlign.center,),
-          ),
+            ),
 
 
     //
@@ -186,7 +176,8 @@ class MyHomePage extends StatelessWidget {
     //               ),))
     //         ],
     //       ),
-        ],
+          ],
+        ),
       ),
 
 
